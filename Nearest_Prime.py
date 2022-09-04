f0ebe788296391171d34b0c0da3ecd43a12a27b7
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-09-04 17:28:28 +0530

    def isprime(n):     count=0     for i in range(2,n//2+1):         if(n%i==0):             count+=1     if(count==0):         return True     else:         return False
    t=int(input())
    for i in range(t):
        
        a=int(input())
        for i in range(a,0,-1):
            if(isprime(i)):
                before=i
                break
        i=a
        while(a>0):
            if(isprime(i)):
                after=i
                break
            i+=1
        if(a-before>after-a):
            print(after)
        else:
            print(before)

---
+++ Nearest_Prime.py
@@ -0,0 +1,29 @@
+def isprime(n):
+    count=0
+    for i in range(2,n//2+1):
+        if(n%i==0):
+            count+=1
+    if(count==0):
+        return True
+    else:
+        return False
+        
+t=int(input())
+for i in range(t):
+    
+    a=int(input())
+    for i in range(a,0,-1):
+        if(isprime(i)):
+            before=i
+            break
+    i=a
+    while(a>0):
+        if(isprime(i)):
+            after=i
+            break
+        i+=1
+    if(a-before>after-a):
+        print(after)
+    else:
+        print(before)
+    
