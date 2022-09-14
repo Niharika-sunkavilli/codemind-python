417aaa91634763ef4fd900d301bcfccfe5ac01cb
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-09-14 21:55:49 +0530

    n=int(input()) a=0 b=1 while(b<n):     c=a+b     a=b     b=c
    if(n-a==b-n):
        print(a,b)
    elif(n-a>b-n):
        print(b)
    else:
        print(a)

--- nearest_fibonacci.py
+++ nearest_fibonacci.py
@@ -1,15 +1,14 @@
 n=int(input())
-a,b=0,1
-while b<n:
+a=0
+b=1
+while(b<n):
     c=a+b
     a=b
     b=c
     
 if(n-a==b-n):
     print(a,b)
-    
-elif(n-a>=b-n):
+elif(n-a>b-n):
     print(b)
 else:
     print(a)
-    
