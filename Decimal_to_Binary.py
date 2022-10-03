a688bf7fea7208fa76e5cc30fd19f2e3ec83fdd1
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-10-03 18:17:30 +0530

    t=int(input()) for i in range(t):     n=int(input())     a=[]     while(n>0):         rem=n%2         a.append(rem)         n=n//2     b=a[::-1]     for i in b:         print(i,end="")     print()

---
+++ Decimal_to_Binary.py
@@ -0,0 +1,13 @@
+t=int(input())
+for i in range(t):
+    n=int(input())
+    a=[]
+    while(n>0):
+        rem=n%2
+        a.append(rem)
+        n=n//2
+    b=a[::-1]
+    for i in b:
+        print(i,end="")
+    print()
+        
