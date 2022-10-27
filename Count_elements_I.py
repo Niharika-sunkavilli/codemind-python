4abc103ea14b394a54d805d4b159d4110b4a01a7
Niharika-sunkavilli <83970877+Niharika-sunkavilli@users.noreply.github.com>
2022-10-27 19:00:37 +0530

    m,n=map(int,input().split()) a=list(map(int,input().split())) b=list(map(int,input().split())) z=[] for i in range(len(a)):     for j in range(len(b)):         if(a[i]==b[j]):             z.append(a[i]) x=set(z) print(len(x))

--- Count_elements_I.py
+++ Count_elements_I.py
@@ -1,14 +1,10 @@
-a,b=map(int,input().split())
-m=list(map(int,input().split()))
-n=list(map(int,input().split()))
-#print(m)
-#print(n)
-a=[]
-c=0
-for i in range(len(m)):
-    for j in range(len(n)):
-        if(m[i]==n[j]):
-            a.append(n[j])
-            
-z=set(a)
-print(len(z))
+m,n=map(int,input().split())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+z=[]
+for i in range(len(a)):
+    for j in range(len(b)):
+        if(a[i]==b[j]):
+            z.append(a[i])
+x=set(z)
+print(len(x))
